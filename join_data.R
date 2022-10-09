9971fb374d93c1d056c16a356a08788dec791831
luaroncrew <ma-kir.ru@yandex.ru>
2022-10-09 12:42:32 +0200

    data join done

--- join_data.R
+++ join_data.R
@@ -30,10 +30,13 @@ names(analysis_dataframe)[length(names(analysis_dataframe))]= 'token_out_symbol'
 names(analysis_dataframe)[7] = 'decimals_token_in'
 analysis_dataframe[9] = NULL
 
+# making all symbols lowercase
 ech = analysis_dataframe$token_in_symbol
 lower = tolower(ech)
-
 analysis_dataframe$token_in_symbol = lower
+ech = analysis_dataframe$token_out_symbol
+lower = tolower(ech)
+analysis_dataframe$token_out_symbol = lower
 
 
 # join the token_id for token_in_symbol
@@ -42,12 +45,42 @@ analysis_dataframe = merge(
   y = token_ids,
   by.x = "token_in_symbol",
   by.y = "token_symbol",
-  all.x = TRUE
+  all.x = TRUE,
+  all.y = FALSE
 )
 
 
-#
+# being given the id, get the price at a day
+analysis_dataframe = merge(
+  x = analysis_dataframe,
+  y = token_ids,
+  by.x = "token_in_symbol",
+  by.y = "token_symbol",
+  all.x = TRUE
+)
 
+analysis_dataframe$token_id.x = NULL
+names(analysis_dataframe)[10] = 'token_in_id'
+
+
+analysis_dataframe = merge(
+  x = analysis_dataframe,
+  y = prices,
+  by.x = c('token_in_id', 'day'),
+  by.y = c('token_id_coingecko', 'day'),
+  all.x = TRUE
+)
+
+# remove all the NULL values
+analysis_dataframe <- na.omit(analysis_dataframe) 
+
+# remove doubled swaps
+analysis_dataframe = unique(analysis_dataframe[c(
+  "receipt_id",
+  "token_in_name",
+  "token_in_symbol",
+  ""
+)])
 
 analysis_dataframe$token_symbol
 
